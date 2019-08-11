89afdd33afa8cc6ae3106f106c64561251a02234
Fernando Boza <hi@fernandoboza.com>
2019-08-11 01:08:45 -0400

    added on search transition

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {HospitalService} from "./services/hospital.service";
-import {Hospital} from "./models/Hospital";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { HospitalService } from "./services/hospital.service";
+import { Hospital } from "./models/Hospital";
 
 @Component({
   selector: 'app-root',
@@ -10,22 +10,35 @@ import {Hospital} from "./models/Hospital";
 })
 export class AppComponent {
 
-  constructor(private hs: HospitalService) {}
+  constructor(private hs: HospitalService) { }
   public lat: number = 25.7308;
   public lng: number = -80.3853;
+  public lattwo: number = 25.8308;
+  public lngtwo: number = -80.4853;
   public search: string;
   public hospitals: Hospital[];
+  public title: string = "Hospital services at a glance";
+  public searchActive: boolean = false;
 
-  ngOnInit(){
-    this.getAllHospital();
+  ngOnInit() {
+    // this.getAllHospital();
   }
 
-  public submitSearch(){
-    console.log(this.search);
+  public submitSearch() {
+    var reg = /^\d+$/;
+    this.title = "Search"
+    this.searchActive = true;
+
+
+    if (reg.test(this.search)) {
+    }
+    else {
+
+    }
   }
 
-  public getAllHospital(){
-    this.hs.getHospitals().subscribe( result => {
+  public getAllHospital() {
+    this.hs.getHospitals().subscribe(result => {
       console.log(result);
       this.hospitals = result;
     });
