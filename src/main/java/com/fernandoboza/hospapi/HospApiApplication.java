78a0fee61f216377f66b50a96bb58e7406103bfb
Fernando Boza <hi@fernandoboza.com>
2019-08-11 00:08:35 -0400

    working on connecting mongo

--- src/main/java/com/fernandoboza/hospapi/HospApiApplication.java
+++ src/main/java/com/fernandoboza/hospapi/HospApiApplication.java
@@ -1,13 +1,18 @@
 package com.fernandoboza.hospapi;
 
+import com.fernandoboza.hospapi.service.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@SpringBootApplication(exclude = {MongoReactiveDataAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication()
+@EnableReactiveMongoRepositories
 public class HospApiApplication {
 
+    @Autowired
+    HospitalService hospitalService;
     public static void main(String[] args) {
         SpringApplication.run(HospApiApplication.class, args);
     }
--- src/main/java/com/fernandoboza/hospapi/config/DataConfig.java
+++ src/main/java/com/fernandoboza/hospapi/config/DataConfig.java
@@ -1,30 +1,33 @@
 package com.fernandoboza.hospapi.config;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.reactivestreams.client.MongoClient;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 
-@Profile(value="local")
 @Configuration
-@Import(EmbeddedMongoAutoConfiguration.class)
 public class DataConfig {
 
-    private static final String DATABASE_NAME = "hospitals";
+//    private static final String DATABASE_NAME = "hospitals";
+//    private static final String  DATABA_URL = "mongodb://<fernando>:<loki1989>@ds261277.mlab.com:61277/hosp-api____";
+//
+//    MongoClient mongoClient = MongoClients.create(new ConnectionString(DATABA_URL));
+//    MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
 
-    @Bean
-    public ReactiveMongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient){
-        return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATABASE_NAME);
-    }
 
-    @Bean
-    public ReactiveMongoOperations reactiveMongoTemplate(ReactiveMongoDatabaseFactory mongoDatabaseFactory){
-        return new ReactiveMongoTemplate(mongoDatabaseFactory);
-    }
+//    @Bean
+//    public ReactiveMongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient){
+//        return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATABASE_NAME);
+//    }
+//
+//    @Bean
+//    public ReactiveMongoOperations reactiveMongoTemplate(ReactiveMongoDatabaseFactory database){
+//        return new ReactiveMongoTemplate(database);
+//    }
 }
