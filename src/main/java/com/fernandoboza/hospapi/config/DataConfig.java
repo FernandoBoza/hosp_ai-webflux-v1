83323d157d4d5cdb636434eec41ca1ce9340d5bf
Fernando Boza <hi@fernandoboza.com>
2019-08-11 13:50:58 -0400

    hooked up the spring app to mongo

--- src/main/java/com/fernandoboza/hospapi/config/DataConfig.java
+++ src/main/java/com/fernandoboza/hospapi/config/DataConfig.java
@@ -1,26 +1,26 @@
-package com.fernandoboza.hospapi.config;
-
-import com.mongodb.ConnectionString;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoDatabase;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
-import org.springframework.data.mongodb.core.ReactiveMongoOperations;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
-
-@Configuration
-public class DataConfig {
-
+//package com.fernandoboza.hospapi.config;
+//
+//import com.mongodb.ConnectionString;
+//import com.mongodb.reactivestreams.client.MongoClient;
+//import com.mongodb.reactivestreams.client.MongoClients;
+//import com.mongodb.reactivestreams.client.MongoDatabase;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+//import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+//import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+//import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
+//
+//@Configuration
+//public class DataConfig {
+//
 //    private static final String DATABASE_NAME = "hospitals";
 //    private static final String  DATABA_URL = "mongodb://<fernando>:<loki1989>@ds261277.mlab.com:61277/hosp-api____";
 //
 //    MongoClient mongoClient = MongoClients.create(new ConnectionString(DATABA_URL));
 //    MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-
-
+//
+//
 //    @Bean
 //    public ReactiveMongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient){
 //        return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATABASE_NAME);
@@ -30,4 +30,4 @@ public class DataConfig {
 //    public ReactiveMongoOperations reactiveMongoTemplate(ReactiveMongoDatabaseFactory database){
 //        return new ReactiveMongoTemplate(database);
 //    }
-}
+//}
